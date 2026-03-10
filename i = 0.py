6216eba2800f401d2f8cfc041319401ef12762c5
Ropsuke <robin.pajula@gmail.com>
2026-03-10 12:22:26 +0200

    kustutasin kogemata lisatud asja
    Removed the random number generation and sorting logic.

--- i = 0.py
+++ i = 0.py
@@ -1,17 +1,1 @@
-import random
-list = []
-for i in range(0, 100):
-    list.append(random.randint(-500, 100))
-n = len(list)
-jah = True
-for kordus in range(n):
-    i = -2
-    d = -1
-    while d > -n:
-        if list[i] < list[d]:
-            i1 = list[i]
-            list[i] = list[d]
-            list[d] = i1
-        i += -1
-        d += -1
-print(str(list))
+
